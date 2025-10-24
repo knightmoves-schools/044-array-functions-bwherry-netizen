d6a68622164d0c7f4e87ce4965037b6896f8f23a
Brendon W. <bwherry@midland.k12.ia.us>
2025-10-24 12:15:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+let automobiles = [['bike', 'helicopter'], ['car', 'truck', 'motorcycle'], ['airplane', 'skateboard']]
+
+let automobilesCopy = automobiles;
+
+
+
+
+document.getElementById("result").innerHTMl = automobiles;
+
+
+
+
+
+
+
+
+
+
+
 
