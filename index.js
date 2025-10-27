16734ba6d9614f7cc92d16026d12df465ba0f9d2
Brendon W. <bwherry@midland.k12.ia.us>
2025-10-27 12:02:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ let automobiles = ['car', 'truck', 'motorcycle'];
 let automobiles.push('airplane', 'skateboard');
 let automobiles.unshift('bike', 'helicopter');
 
-//let automobilesCopy = automobiles;
-//let automobiles.pop('helicopter');
-//let automobiles.shift('bike');
+let automobilesCopy = automobiles;
+let automobiles.pop('helicopter');
+let automobiles.shift('bike');
 
 
 
 
-document.getElementById("result").innerHTMl = automobiles;
+document.getElementById("result").innerHTML = automobiles;
+
